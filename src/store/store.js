5cad3bb776f42bd1b246fab3eeddbcc1e37617fb
evnayd <vjane87@yahoo.com>
2021-06-19 23:21:12 +0200

    changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,5 @@
 import { createStore } from "vuex";
 
-//api key 31abf315e391d1824b56e0f87a86abe2
-//token 981204274b99921f0ca0180b6690e6da4cdcf34fbd246f064dc9efd4b60f051d
-
 export const store = createStore({
   state: {
     boards: [],
